4fbb48c362952ca73ec0718b9a33a889f16407ba
sneubig <neubig.stephen@gmail.com>
2014-12-14 11:03:51 -0500

    appended random words on button click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-$(function () {
+$(function() {
     $(".button").on('click', function(){
+    	console.log("working!");
     	$(this).toggleClass('active');
+
+    	var words = ["fun", "rain", "christmas"];
+    	var rand = words[Math.floor(Math.random() * words.length)];
+    	
+    	if ('.active'){
+    		console.log(rand);
+    		$( "<p>" + rand + "</p>" ).appendTo( "#paper" );
+    	} else{
+    		console.log("not working!");
+    	};
+
+    
+
+
+  		// $( ".conky" ).toggle( "bounce", { times: 3 }, "slow" );
+
     });
+		
+
 });
