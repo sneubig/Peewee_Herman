f49b147b9440e56ac557e771f74e69339c78e500
sneubig <neubig.stephen@gmail.com>
2014-12-14 11:53:32 -0500

    added getSecretWord function

--- js/script.js
+++ js/script.js
@@ -2,25 +2,29 @@ $(function() {
     $(".button").on('click', function(){
     	// console.log("working!");
     	$(this).toggleClass('active');
-
-   //  	var audio = document.getElementById("conky-sound");
-			// audio.play();
+ 			
+ 			// Conky Sound Clip
 			document.getElementById('conky-sound').play();
 
+			getSecretWord();
+
+    });
+
+    function getSecretWord(){
     	var words = ["fun", "rain", "christmas"];
     	var rand = words[Math.floor(Math.random() * words.length)];
     	
     	if ('.active'){
     		console.log(rand);
     		$( "<p>" + rand + "</p>" ).appendTo( "#paper" );
-
-    		$( ".conky" ).toggle( "bounce", { times: 3 }, "slow" );
+    		
     	} else{
     		console.log("not working!");
     	};
-
-
-    });
+    }
 		
 
 });
+
+
+
